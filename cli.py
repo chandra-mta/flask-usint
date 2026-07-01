da4f37172010a4e15662ac91cb5540304f9afaf6
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-07-01 12:31:20 -0400

    document that the cli needs the correct conda environment invoked for the given app installation.

--- cli.py
+++ cli.py
@@ -2,6 +2,12 @@
 """
 Python script to act as the command line interface for this flask-usint application installation.
 Note that the commands are installation specific, and will affect only what this directory's application can control.
+
+Note that this script requires the conda environment supporting this directory's application to be activate.
+The current conda environments supporting the different application installations can be found at 
+```
+/proj/sot/mta/envs
+```
 """
 from cli.main import cli
 if __name__ == "__main__":
