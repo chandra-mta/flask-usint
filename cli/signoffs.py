8b17c1a3f04e405920e91c3d2a38e9e8e368084e
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-09-21 13:34:38 -0400

    add categorization method to provided ordering for when reminder emails are sent.

--- cli/signoffs.py
+++ cli/signoffs.py
@@ -31,6 +31,13 @@ SIGNOFF_SUBJECTS = {
     'usint_status': "Updates needed for obsid.revs (USINT sign-off)"
 }
 
+_OTHER_PENDING_COLUMNS = {
+    'usint_status' : ('hrc_si_status', 'acis_si_status', 'acis_status', 'general_status'),
+    'hrc_si_status' : ('acis_si_status', 'acis_status', 'general_status'),
+    'acis_si_status' : ('acis_status', 'general_status'),
+    'acis_status' : ('general_status'),
+}
+
 def _fetch_by_column_and_status(column_model, status_value):
     """
     Using one of the five SIGNOFF_COLUMN dictionary values, we query and select all values for that column matching the given status.
@@ -78,6 +85,42 @@ def _fetch_all_pending():
         pending_results[column_name] = pending_list
     return pending_results
 
+def _categorize_pending_signoffs(pending_results):
+    """
+    Some signoffs are pending in multiple columns, but we need them in a specific order.
+    This function parses the pending signoffs and drops them from a column's list if they are also pending
+    in a previous signoff column.
+    """
+    categorized_pending = {
+        'general_status': pending_results['general_status'],
+        'acis_status': [],
+        'acis_si_status': [],
+        'hrc_si_status': [],
+        'usint_status': []
+    }
+
+    def _other_pending(signoff, check_list):
+        """
+        Iterate over the ORM columns provided. If one is pending, return True
+        """
+        other_pending = False
+        for name, value in models.iter_columns(
+            signoff, check_list
+        ):
+            if value=='Pending':
+                other_pending = True
+        return other_pending
+
+    for column_category, check_list in _OTHER_PENDING_COLUMNS.items():
+        #: Check each status column for whether the previous columns are still pending.
+        for signoff in pending_results[column_category]:
+            other_pending = _other_pending(signoff, check_list)
+            if not other_pending:
+                #: Then include in the matching column category
+                categorized_pending[column_category] += signoff
+
+    return categorized_pending    
+
 def _construct_group_reminder_content(column, pending_list):
     """
     Construct the reminder email content using the pending signoffs list.
@@ -99,14 +142,15 @@ def send_reminder_emails():
     Send a reminder email for all pending signoffs across all columns.
     """
     pending_results = _fetch_all_pending()
-
+    categorized_pending = _categorize_pending_signoffs(pending_results)
+    #print(categorized_pending)
     count = 0
-    for column, pending_list in pending_results.items():
+    for column, pending_list in categorized_pending.items():
         count += len(pending_list)
 
     if count > 0:
         column = 'general_status'
-        gen_content =_construct_group_reminder_content(column, pending_results[column])
+        gen_content =_construct_group_reminder_content(column, categorized_pending[column])
         to = 'william.aaron@sao.si.edu'
         emailing.send_email(gen_content, SIGNOFF_COLUMNS[column], to=to)
         
