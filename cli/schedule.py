f372208e03fd9863dac34b1d2df1201c14e3e5e4
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-09-22 17:47:26 -0400

    format schedule database maintenance

--- cli/schedule.py
+++ cli/schedule.py
@@ -4,6 +4,7 @@ Database schedule table interface commands
 import click
 from .core import with_app_context, db, models
 from datetime import datetime, timedelta
+from sqlalchemy import select
 
 _FUTURE_MONTHS = 8
 _PERIOD_DAYS = 6 #: Numerically 13 in as time period end points are inclusive.
@@ -13,21 +14,21 @@ _PERIOD_DAYS = 6 #: Numerically 13 in as time period end points are inclusive.
 #: 52|32||2027-02-15 00:00:00.000000|2027-02-21 00:00:00.000000|
 #
 
-@click.command("maintain-schedule")
-@with_app_context
-def maintain_schedule():
-    """Inject additional schedule time period entries into the schedule table up to a point in the future"""
+_NOW = datetime.now()
 
-    latest = (
-        models.Schedule.query
-        .order_by(models.Schedule.order_id.desc())
-        .first()
-    )
+def _inject_schedule_entries():
+    """
+    Inject additional schedule time period entries into the schedule table up to a point in the future
+    Must run in an app context.
+    """
+
+    query = select(models.Schedule).order_by(models.Schedule.order_id.desc())
+    latest = db.session.execute(query).scalars().first()
 
     if latest is None:
         return
 
-    horizon = datetime.now() + timedelta(days=30 * _FUTURE_MONTHS)
+    horizon = _NOW + timedelta(days=30 * _FUTURE_MONTHS)
 
     previous = latest
     order_id = latest.order_id
@@ -51,3 +52,11 @@ def maintain_schedule():
         previous = new_entry
 
     db.session.commit()
+
+@click.command("maintain-schedule")
+@with_app_context
+def maintain_schedule():
+    """
+    Run all period schedule database maintenance functions
+    """
+    _inject_schedule_entries()
